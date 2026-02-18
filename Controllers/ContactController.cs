c26223c7918a8290833bccb3114d5a64e7cce9d0
maheshwariaditya901-coder <maheshwariaditya901@gmail.com>
2026-02-18 15:45:43 +0530

    make first asp.net application

--- Controllers/ContactController.cs
+++ Controllers/ContactController.cs
@@ -1,12 +1,45 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.ViewEngines;
+    using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
-{
-    public class ContactController : Controller
     {
-        public IActionResult Index()
+        public class ContactController : Controller
         {
-            return View();
+
+            private readonly string email = "contact112@gmail.com";
+            private readonly string address = "New Delhi";
+            public IActionResult Index()
+            {
+   
+                // both are used for transfering data from controller to view.
+
+                                     //ViewBag
+                                     //Dynamic object
+                                     //Easier and cleaner syntax
+                                     //No type casting needed
+                                     //Built on top of ViewData
+
+                ViewData["EmailAddress"] = email;
+                ViewBag.Address = address;
+
+                return View();  
+            }
+
+            [HttpPost] // this tell the asp.net that run this method when form is submitted
+            public IActionResult Index(ContactModel model)
+            {
+            // here we do  Model Binding.
+            ViewData["EmailAddress"] = email;       
+                ViewBag.Address = address;
+
+                if (!ModelState.IsValid)
+                {
+                    return View(model);
+                }
+                ViewBag.SuccessMessage = "you submitted the form";
+                ModelState.Clear();
+                return View();
+            }
         }
     }
-}
---
+++ Models/ContactModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class ContactModel
+    {
+
+        [Required(ErrorMessage = "enter the first name correct ")]
+        [StringLength(50)]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "enter the Last name correct ")]
+        [StringLength(50)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email Address")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(500)]
+        [Display(Name = "Message")]
+        public string Message { get; set; }
+    }
+}
